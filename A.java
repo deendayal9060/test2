9c5bf3699b5fc59ccdbb316bf06419f69ba622be
deendayal9060 <ravipa003@gmail.com>
2023-09-20 09:17:09 +0530

    first

---
+++ A.java
@@ -0,0 +1,12 @@
+class A
+{
+	public static void main(String[] args) 
+	{
+		System.out.println(100 > 40);
+		System.out.println(100 > 140);
+		System.out.println(100 > 100);
+		System.out.println(1000 > 140);
+		System.out.println(10023 > 140);
+		System.out.println(1002 > 140);
+	}
+}
---
+++ B.java
@@ -0,0 +1,10 @@
+class B 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println(60 > 200);
+		System.out.println(600 > 200);
+		System.out.println(60 > 20);
+		System.out.println(6 > 20);
+	}
+}
